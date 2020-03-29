6ba47a48c4db926e497312080e5fad54200ad0fc
ahofer <Alexander.Hofer@abf.ch>
2020-03-30 00:01:55 +0200

    Test

---
+++ toggeli/precache-manifest.ad876faf1bbfe6aa00b5482bd539f2aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0361d2d2936f5bfa29dcd827905de1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f91080bdb77e8f2b94c4",
+    "url": "/static/css/main.9559e866.chunk.css"
+  },
+  {
+    "revision": "33063c6fab03dae9a854",
+    "url": "/static/js/2.25ba08aa.chunk.js"
+  },
+  {
+    "revision": "0e84afde8a138d28ddc413c2c1367a0a",
+    "url": "/static/js/2.25ba08aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91080bdb77e8f2b94c4",
+    "url": "/static/js/main.90bfb75f.chunk.js"
+  },
+  {
+    "revision": "69e7e0d6db362ddf6c01",
+    "url": "/static/js/runtime-main.f5b96cdb.js"
+  }
+]);
